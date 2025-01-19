abe440a3a54c8a3a1c2adb48b6f6d15f46de2b75
Haydal810 <haydal.tmps@gmail.com>
2025-01-19 20:20:47 +0000

    site revisions as per client request

--- dark-agency/js/script.js
+++ dark-agency/js/script.js
@@ -261,3 +261,14 @@ function sendMail(contact) {
         );
     return false; // To block from loading a new page...
 }
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
