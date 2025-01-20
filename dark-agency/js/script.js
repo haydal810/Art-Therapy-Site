b4a8751c89e04f00078fe3498299a637e08e47af
Haydal810 <haydal.tmps@gmail.com>
2025-01-20 22:36:48 +0000

    landing section on home page amended to slide show

--- dark-agency/js/script.js
+++ dark-agency/js/script.js
@@ -262,13 +262,13 @@ function sendMail(contact) {
     return false; // To block from loading a new page...
 }
 
-$("#slideshow > div:gt(0)").hide();
+// $("#slideshow > div:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > div:first')
-  .fadeOut(1000)
-  .next()
-  .fadeIn(1000)
-  .end()
-  .appendTo('#slideshow');
-}, 3000);
+// setInterval(function() { 
+//   $('#slideshow > div:first')
+//   .fadeOut(1000)
+//   .next()
+//   .fadeIn(1000)
+//   .end()
+//   .appendTo('#slideshow');
+// }, 3000);
